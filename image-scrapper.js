e2521d1ea16a0c3a16c9c6339758d40520ce2771
Vijay Gupta <vijayrauniyar1818@gmail.com>
2023-03-05 14:57:41 +0530

    added some changes

--- image-scrapper.js
+++ image-scrapper.js
@@ -17,8 +17,15 @@ async function imageScrapper(websiteUrl, pathName) {
         if (imgUrls.length > 0) {
             console.log(`Found ${imgUrls.length} images on the website`);
             createDirIfNotExists(downloadDirectory);
+            let count = 0;
             for (const imgUrl of imgUrls) {
-                await downloadImage(imgUrl, downloadDirectory);
+                try {
+                    console.log('Downloading image : ' + count);
+                    await downloadImage(imgUrl, downloadDirectory);
+                    count++;
+                } catch (error) {
+                    console.log(`Error downloading image : ${error.message}`);
+                }
             }
         } else {
             console.log('No images found on the website');
@@ -26,6 +33,7 @@ async function imageScrapper(websiteUrl, pathName) {
     } catch (error) {
         console.log(`Error fetching website: ${error.message}`);
     }
+    console.log("Done");
 };
 
 function fetchData(urlString) {
@@ -50,7 +58,10 @@ function extractImgUrls(html) {
     const regex = /<img [^>]*src=(?:(?:'([^']*)')|(?:"([^"]*)"))/gi;
     let match;
     while ((match = regex.exec(html)) !== null) {
-        imgUrls.push(match[1] || match[2]);
+        if (match[1])
+            imgUrls.push(match[1]);
+        if (match[2])
+            imgUrls.push(match[2]);
     }
     return imgUrls;
 }
@@ -64,6 +75,8 @@ function createDirIfNotExists(dirPath) {
 function downloadImage(imgUrl, downloadDirectory) {
     return new Promise((resolve, reject) => {
         const imgUrlParts = url.parse(imgUrl);
+        if (!imgUrlParts.pathname)
+            return;
         const imgFilename = path.basename(imgUrlParts.pathname);
         const imgPath = path.join(downloadDirectory, 'image ' + imgFilename.replace(/[^a-zA-Z ^.]/g, ""));
 
@@ -79,7 +92,6 @@ function downloadImage(imgUrl, downloadDirectory) {
             response.pipe(fileStream);
 
             fileStream.on('finish', function () {
-                console.log(`Image "${imgFilename}" downloaded`);
                 resolve();
             });
 
